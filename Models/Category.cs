17c18008aa0cc74129b457e6e5f0f2be4aed8838
HueiRu <4a090036@stust.edu.tw>
2021-09-01 00:18:16 +0800

    新增model

---
+++ Models/Category.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Homework_SkillTree.Models
+{
+	//public class CategoryListVM
+	//{
+	//	public IList<CategoryInputVM> CategoriesInputVM { get; set; }
+	//}
+
+	public enum MoneyEnum
+	{
+		支出,
+		收入
+	}
+	public class CategoryInputVM
+	{
+		[Display(Name = "#")]
+		public int id { get; set; }
+		[Display(Name = "類別")]
+		public MoneyEnum Categories { get; set; }
+		[Display(Name = "金額")]
+		public int Money { get; set; }
+		[Display(Name = "日期")]
+		[DataType(DataType.Date)]
+		public DateTime Date { get; set; }
+		[Display(Name = "備註")]
+		public string Note { get; set; }
+	}
+}
